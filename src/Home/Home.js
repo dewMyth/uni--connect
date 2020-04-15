a7206d39f9e594761c3b0a62a5b5b35df9f76d73
dewMyth <akalankadewmith@gmail.com>
2020-04-15 17:06:03 +0530

    First  Commit

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css"
+
+const Home = () => {
+  return(
+
+    <div className="container">
+        <div>
+            Hello World
+        </div>
+
+    </div>
+  )
+}
+export default Home;
